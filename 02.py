e34967d4f77ea19f5148252dd31a8862b97e3edd
Soudehm <satkinu@gmail.com>
2016-07-05 02:56:35 -0700

    Add files via upload

--- 02.py
+++ 02.py
@@ -10,6 +10,20 @@ os.chdir('/Users/Soudehm/Documents/July4thWTW/WBS')
 directory = "/Users/Soudehm/Documents/July4thWTW/DataBaseOfMyCloset"
 wb = openpyxl.load_workbook('WTW_DB.xlsx')
 
+#sheet_data = wb.active
+#sheet_data.title = 'WTW_DB_data'
+#sheet_data.sheet_properties.tabColor = "1072BA"
+
+sheet_data = wb.get_sheet_by_name ('WTW_DB_data')
+sheet_data.sheet_properties.tabColor = "107155"
+
+#workplace = wb.create_sheet() # insert a sheet at the end
+#workplace.title = 'WTW_DB'
+#workplace.sheet_properties.tabColor = "BA72BA"
+
+workplace = wb.get_sheet_by_name('WTW_DB')
+workplace.sheet_properties.tabColor = "BA154A"
+
 #------------------------------------------------------------------------------
 # this code was modified accordingly http: //stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python
 
@@ -32,15 +46,12 @@ def get_filepaths(directory):
 
     return file_paths  # Self-explanatory.
 #------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Run the above function and store its results in a variable.
 full_file_paths = get_filepaths(directory)
-
-sheet_data = wb.active
-sheet_data.title = 'WTW_DB_data'
-sheet_data.sheet_properties.tabColor = "1072BA"
-
+# filling the sheet_data using this
 for index_files in range(len(full_file_paths)):
-    sheet_data['A' + str(index_files + 1)].value = full_file_paths [index_files]
+    sheet_data['B' + str(index_files + 3)].value = full_file_paths [index_files]
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
@@ -58,19 +69,24 @@ def get_filenames(directory):
     
     # Walk the tree.
     for root, directories, files in os.walk(directory):
-        file_names.append(files)
-
+        file_names.extend(files)
+        break
+    
     return file_names  # Self-explanatory.
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#print(len(full_file_paths))
+#print(len(full_file_names))
+#------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 # Run the above function and store its results in a variable.
 full_file_names = get_filenames(directory)
-#print(full_file_names)
 
-workplace = wb.create_sheet() # insert a sheet at the end
-workplace.title = 'WTW_DB'
-workplace.sheet_properties.tabColor = "BA72BA"
-
+# filling the sheet_data using this
 for index_files in range(len(full_file_names)):
-    workplace['A' + str(index_files + 1)].value = full_file_names[index_files]
+    workplace['B' + str(index_files + 3)].value = full_file_names [index_files]
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 wb.save('WTW_DB.xlsx')
